2ff4fe461b9012d89e65414f5636bcec956f35bc
BlindBlackHole <58915802+BlindBlackHole@users.noreply.github.com>
2020-04-08 13:59:30 +0300

    Update Main.java

--- Gerasimenko/Модуль1.2/src/com/company/Main.java
+++ Gerasimenko/Модуль1.2/src/com/company/Main.java
@@ -18,7 +18,11 @@ public class Main {
                         flag = false;
                     }
                 }else{
-                    flag = false;
+                    if (n <= 0) {
+                        System.out.println("Введите целое число от 1 до 100!");
+                    } else {
+                        flag = false;
+                    }
                 }
             } else {
                 System.out.println("Введите целое число от 1 до 10!");
@@ -66,12 +70,12 @@ public class Main {
             arr1 = setArr(n1);
         }else {
             for (int i = 0; i < n1; i++) {
-                arr1[i] = (int) (Math.random() * 100);
+                arr1[i] = (int) (Math.random() * 50);
             }
         }
         showArr(arr1);
 
-        System.out.println("Выберите метод заполнения массива2: ");
+        System.out.println("Выберите метод заполнения массива2(чтоб все работало, нужно чтоб элементы этого массива было больше первого): ");
         System.out.println("Введите 1 для заполения вручную\nВведите любую строку для автозаполнения\n");
         String s2;
         s2 = in.next();
@@ -79,46 +83,43 @@ public class Main {
             arr2 = setArr(n2);
         }else {
             for (int i = 0; i < n2; i++) {
-                arr2[i] = (int) (Math.random() * 100);
+                arr2[i] = (int) (Math.random() * 50 + 50);
             }
         }
         showArr(arr2);
-
-        int max,min;
-
-        if(arr2[0] > arr1[n1-1]){
-            max = arr2[0];
-            min = arr1[n1-1];
-        }else{
-            min = arr2[0];
-            max = arr1[n1-1];
-        }
-
-        int[] temp = new int[n1+n2];
-        int count = 0;
-        for(int i=0; i<n1; i++){
-            if(arr1[i] > max || arr1[i] < min)
-                continue;
-            temp[count] = arr1[i];
-            count++;
-        }
-        for(int i=0; i<n2; i++){
-            if(arr2[i] > max || arr2[i] < min)
-                continue;
-            temp[count] = arr2[i];
-            count++;
-        }
-        showArr(temp);
-
-        System.out.println("Найденые элементы: ");
-        for(int i=0; i<n2; i++) {
-            for (int j = 0; j < temp.length; j++) {
-                if(temp[j] == 0)
-                    continue;
-                if (arr2[i] % temp[j] == 0) {
-                    System.out.print(temp[j] + " ");
+        int min = arr1[n1-1];
+        int max = arr2[0];
+        int [] temp = new int [(max-min)*n1];
+        int count =0;
+        boolean flag = true;
+        for(int i=min; i<max; i++){
+            flag = true;
+            for(int j=0; j<n1; j++){
+                if(i % arr1[j] != 0){
+                    flag = false;
                 }
             }
+            if(flag){
+                temp[count] = i;
+                count++;
+            }
+        }
+
+        for(int i=0; i<n2; i++){
+            for(int j=0; j<count; j++){
+                if(temp[j] == 0)
+                    continue;
+                if(arr2[i] % temp[j] != 0){
+                    temp[j] = 0;
+                }
+            }
+        }
+
+        System.out.println("Найденые элементы: ");
+        for(int i=0; i<temp.length; i++) {
+            if(temp[i] == 0)
+                continue;
+            System.out.print(temp[i] + " ");
         }
 
     }
