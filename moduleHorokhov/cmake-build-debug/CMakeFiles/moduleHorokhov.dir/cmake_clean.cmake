324ebb70359812532eaf4986b423d75cc4f978ec
Daniel Gorokhov <55414689+monidan@users.noreply.github.com>
2020-04-08 10:02:06 +0300

    Created branch and project.

---
+++ moduleHorokhov/cmake-build-debug/CMakeFiles/moduleHorokhov.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/moduleHorokhov.dir/main.cpp.o"
+  "libmoduleHorokhov.dll.a"
+  "moduleHorokhov.exe"
+  "moduleHorokhov.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/moduleHorokhov.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
