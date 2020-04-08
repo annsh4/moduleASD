91d5028eac1d304b9603bad0fba13f80d043c63c
Daniel Gorokhov <55414689+monidan@users.noreply.github.com>
2020-04-08 12:56:39 +0300

    Ended coding 'Task #1'

--- moduleHorokhov/task1/Matrix.h
+++ moduleHorokhov/task1/Matrix.h
@@ -5,24 +5,25 @@
 #ifndef MODULEHOROKHOV_MATRIX_H
 #define MODULEHOROKHOV_MATRIX_H
 
+#include <iostream>
 
 class Matrix {
-    int rowCount, columnCount;
-    bool assigned;
-    int** a = new int*[rowCount];
+    int rowCount;
+    int columnCount;
 
     void deleteMatrix();
-    bool isFilled();
+
+    friend std::ostream &operator<< (std::ostream &out, Matrix &output);
 public:
+    int **matrix;
     Matrix();
     Matrix(int columns, int rows);
     ~Matrix();
 
 
-    void fillMatrixAuto();
-    void fillMatrixByYourself(int key_number, int rowCounter, int columnCounter);
-    void calculateDifference();
-    void displayMatrix();
+    void fillMatrixAuto(Matrix &filledMatrix);
+    void fillMatrixByYourself(int key_number, int rowCounter, int columnCounter, Matrix &filledMatrix);
+    int calculateDifference(Matrix &calcMatrix);
 };
 
 
